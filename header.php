01a493aa2f19b5f493050bbc78c00974914cecd0
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-05-27 13:45:57 -0500

    Sessions

--- header.php
+++ header.php
@@ -7,13 +7,13 @@ try{
     echo $e->getMessage();
 }
 include('classes.php');
-define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../../../phpBB3';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-include($phpbb_root_path . 'common.' . $phpEx);
+    define('IN_PHPBB', true);
+    $phpbb_root_path = '../../../phpBB3';
+    $phpEx = substr(strrchr(__FILE__, '.'), 1);
+    include($phpbb_root_path . 'common.' . $phpEx);
 
-// Start session management
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup();
+    // Start session management
+    $user->session_begin();
+    $auth->acl($user->data);
+    $user->setup(); 
 ?>
