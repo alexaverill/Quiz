0436aa749bfdf42d21b1b952cf007c5e5edd1b54
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-05-27 13:42:24 -0500

    Session location

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ try{
 }
 include('classes.php');
 define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '/var/www/sites/scioly.org/public/phpBB3';
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../../../phpBB3';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 
