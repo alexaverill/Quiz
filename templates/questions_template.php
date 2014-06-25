6844323bf9b1286327fc8e4dc47d46f81487f766
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-06-24 19:44:05 -0500

    More style changes

--- templates/questions_template.php
+++ templates/questions_template.php
@@ -5,21 +5,23 @@ if (is_numeric($_GET['event'])){
     $event =  $_GET['event'];
 }
 $question = new Questions;
+$name = $question->get_event($event);
+echo '<h1>'.$name.'</h1>';
 if($_POST['check']){
     if($_POST['type']==1 || $_POST['type']==3){
         if($question->check_mc($_POST['idval'],$_POST['response'])){
-            echo '<h2>Correct!</h2>';
+            echo '<h2>Correct!</h2><br/>';
         }else{
-            echo '<h2>Incorrect</h2>';
+            echo '<h2>Incorrect</h2><br/>';
         }
     }else if($_POST['type']==2){
         if($question->check_short($_POST['idval'],$_POST['response'])){
-            echo '<h2>Correct!</h2>';
+            echo '<h2>Correct!</h2><br/>';
         }else{
-            echo '<h2>Incorrect</h2>';
+            echo '<h2>Incorrect</h2><br/>';
         }        
     }else{
-        echo "There was an error grading";
+        echo "There was an error grading<br/>";
     }
     $name = $question->get_event($event);
     echo "<h2>$name</h2>";
