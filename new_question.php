a26364ef2e7ad57511538830984cd23dcd87ca2e
Alex <alex.averill2013@gmail.com>
2014-07-12 14:05:59 -0500

    Testing

--- new_question.php
+++ new_question.php
@@ -4,13 +4,15 @@ if($_POST['MCQuestion']){
     $Question = new Questions;
     $input = htmlspecialchars($_POST['inputquest']);
     //echo  $_FILES['userfile']['name'];
+    echo $_POST['URL'];
+    echo length($_POST['URL']);
     $usID=$user->data['user_id'];
     if($_FILES['userfile']['name']>1){
         $file = new files;
         $imageLocation=$file->upload($_FILES['userfile']['name'],$_FILES['userfile']['size'],$_FILES['userfile']['tmp_name'],$_FILES['userfile']['type']);
         $Question->add_question($_POST['event'],$input,$_POST['option1'],$_POST['option2'],$_POST['option3'],$_POST['option4'],$_POST['option5'],$_POST['correct_answer'],$imageLocation,3,NULL,$usID);
     } else if($_POST['URL']>1){
-        echo 'test';
+        echo 'EEVIL';
          $file = new files;
         $imageLocation=$file->pull_image($_POST['URL']);
         $Question->add_question($_POST['event'],$input,$_POST['option1'],$_POST['option2'],$_POST['option3'],$_POST['option4'],$_POST['option5'],$_POST['correct_answer'],$imageLocation,3,NULL,$usID);
