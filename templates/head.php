30145532fee580b5d2cee5d17923cb64dee4a7f3
Alex <alex.averill2013@gmail.com>
2014-07-17 22:57:22 -0500

    Nav fixes

--- templates/head.php
+++ templates/head.php
@@ -29,7 +29,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html"><img src="http://scioly.org/logo-white.png" height="50px" width="170px"/></a>
+          <a class="navbar-brand" href="index.php"><img src="http://scioly.org/logo-white.png" height="50px" width="170px"/></a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
