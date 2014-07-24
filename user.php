203f1936bedfb0746d2b6a53eec734318253c719
Alex <alex.averill2013@gmail.com>
2014-07-24 00:03:02 -0500

    meh

--- user.php
+++ user.php
@@ -1,9 +1,18 @@
 <?php
 include('header.php');
 if($loggedIn){
-    echo $user->data['user_id'];
-$Display = new Display;
-$Display->template('users.php');
+    $usID =  $user->data['user_id'];
+?>
+<h3><?php $user->data['username'];?> Stats</h3>
+
+<h4>Question Statistics</h4>
+Questions Added:<?php $user->total_submitted($usID);?>
+Questions Anwnsers:
+Top Event:
+<h4>Questions Submitted</h4>
+<div class=scroll_container>
+</div>
+<?php
 }else{
     echo '<br/><a href="../ucp.php?mode=login">Please Login to view your stats.</a>';
 }
