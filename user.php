0c194e9e2ceca22c45db1a1d2c75ac708af70a41
Alex <alex.averill2013@gmail.com>
2014-07-23 23:48:43 -0500

    template fix.

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
 include('header.php');
 if($loggedIn){
 $Display = new Display;
-$Display->template('user.php');
+$Display->template('users.php');
 }else{
     echo '<br/><a href="../ucp.php?mode=login">Please Login to view your stats.</a>';
 }
