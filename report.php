35872dd2e0bd6c106065e8953263f0b445447196
Alex <alex.averill2013@gmail.com>
2014-08-10 17:28:01 -0500

    wat

--- report.php
+++ report.php
@@ -6,7 +6,7 @@ $questionID = $_GET['Qid'];
 if($_POST['report']){
     $quests = new Questions;
     $quests->report_question($questionID,$_POST['reportText'],$user->data['user_id']);
-    echo 'Report Fixed, return to the <a href="index.php>home page</a>';
+    echo '<a href="index.php>home page</a> Report Fixed, return to the <a href="index.php>home page</a>';
     die();
 }
 ?>
