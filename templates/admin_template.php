4374aa5154d039f55e93ad022c52f8017d68b420
Alex <alex.averill2013@gmail.com>
2014-08-10 16:46:17 -0500

    layout fixes

--- templates/admin_template.php
+++ templates/admin_template.php
@@ -1,18 +1,20 @@
+<div>
 <h2>Approve Questions:</h2>
 <?php
 $admin = new AdminQuestions;
 $admin->pending_questions();
 ?>
-<br/>
+</div>
+<div>
 <h2>Open Reports:</h2><br/>
 <?php
 $display = new Display;
 $display->pull_reports();
 ?>
-<br/>
-<h2>Reset Event Numbering</h2>
-Use this to fix multiple questions showing up on a single event
-<form method="POST" action="">
+</div>
+<div><h2>Reset Event Numbering</h2></div>
+<div>Use this to fix multiple questions showing up on a single event</div>
+<form method="POST" action="" class="inline-form">
 <select name="event">
 <?php
 $form = new Forms;
