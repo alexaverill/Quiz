77cec1a0ed20e77c2b22794e1c5ed7a4fd1009f3
Alex <alex.averill2013@gmail.com>
2014-08-10 16:15:31 -0500

    Template fix

--- templates/admin_template.php
+++ templates/admin_template.php
@@ -1,10 +1,6 @@
 <h2>Approve Questions:</h2>
-<?php
-$admin = new AdminQuestions;
-$admin->pending_questions();
-?>
 <br/>
 <h2>Open Reports:</h2><br/>
 <?php
-$admin->pull_reports();
+$display->pull_reports();
 ?>
