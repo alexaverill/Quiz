633756852633e86e92fafcc0c04e291cc0684861
Alex <alex.averill2013@gmail.com>
2014-08-11 00:06:37 -0500

    failure to spell

--- leaders.php
+++ leaders.php
@@ -3,8 +3,9 @@ include('header.php');
 
 $stats = new stats;
 $numberDisplay = 5;
-$sumitted = $stats->return_submitted_stats($numberDisplay);
-$correct = $stats->return_total_correct_stats($numberDisplay);
-echo $twig->render('leaders.html',array('TopSubmitters'=>$sumitted,'TopCorrect'=>$correct));
+$submitted = $stats->return_submitted_stats($numberDisplay);
+$submitted= $submitted->fetchAll();
+ $correct = $stats->return_total_correct_stats($numberDisplay);
+echo $twig->render('leaders.html',array('TopSubmitters'=>$submitted,'TopCorrect'=>$correct));
 ?>
 
