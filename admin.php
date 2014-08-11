5ae65642805de8994873a277ff32ff3bdc1529bb
Alex <alex.averill2013@gmail.com>
2014-08-10 23:35:00 -0500

    Fixing the namestatus

--- admin.php
+++ admin.php
@@ -34,7 +34,7 @@ if($user->data['group_id']==5 ||$user->data['group_id'] ==4|| $user->data['group
     $reported = $adminQuestions->pull_reports();
     $questions = $adminQuestions->query_questions();
     $eventList = $question->return_all_events();
-    if($questions['eventid']>0){
+    if($questions[0]['eventid']>0){
         $NameStatus = 1;
     }
     echo $twig->render('adminHTML.html',array('ReportArray'=>$reported,'ApprovalArray'=>$questions,'Event'=>$NameStatus,'EventList'=>$eventList));
