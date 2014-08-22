b3a5b5da3085fdedcaef23fdaae7fc1ff7f4f557
Alex <alex.averill2013@gmail.com>
2014-08-22 11:00:34 -0500

    Parenthesis were off

--- classes.php
+++ classes.php
@@ -652,7 +652,7 @@ class AdminQuestions extends Questions{
         foreach($reportArray as $data){
             $returnArray[0]['Report'] = $data['report'];
             $returnArray[0]['user'] = $user->rationalize_userID($data['userID']);
-            array_push($returnArray[0],$this->select_question($data['questionID']);
+            array_push($returnArray[0],$this->select_question($data['questionID']));
         }
         return $returnArray;
     }
